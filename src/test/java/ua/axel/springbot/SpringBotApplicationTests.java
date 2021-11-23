224942d25576c65754ae28b940c280d5f21096d7
AxelNordov <iq@ua.fm>
2021-11-23 12:35:26 +0200

    Spring Initializr

---
+++ src/test/java/ua/axel/springbot/SpringBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.axel.springbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4593b606132d99ae9383aeb93b72f0ae6b85ad81
AxelNordov <iq@ua.fm>
2021-11-23 14:09:52 +0200

    add "Hello, SpringBot!"

--- src/main/java/ua/axel/springbot/bot/SpringBot.java
+++ src/main/java/ua/axel/springbot/bot/SpringBot.java
@@ -3,7 +3,9 @@ package ua.axel.springbot.bot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class SpringBot extends TelegramLongPollingBot {
@@ -16,7 +18,15 @@ public class SpringBot extends TelegramLongPollingBot {
 
 	@Override
 	public void onUpdateReceived(Update update) {
-		//
+		try {
+			SendMessage sendMessage = SendMessage.builder()
+					.chatId(update.getMessage().getChatId().toString())
+					.text("Hello, SpringBot!")
+					.build();
+			execute(sendMessage);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override

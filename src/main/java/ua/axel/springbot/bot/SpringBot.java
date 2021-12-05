a92add4f0261a9b3b9d8fe019617e4e7bcb724ea
AxelNordov <iq@ua.fm>
2021-12-05 19:37:44 +0200

    wrap exception

--- src/main/java/ua/axel/springbot/bot/SpringBot.java
+++ src/main/java/ua/axel/springbot/bot/SpringBot.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ua.axel.springbot.service.MessageService;
@@ -26,13 +28,17 @@ public class SpringBot extends TelegramLongPollingBot {
 
 	@Override
 	public void onUpdateReceived(Update update) {
-		messageService.getMethod(update).ifPresent(method -> {
-			try {
-				execute(method);
-			} catch (TelegramApiException e) {
-				e.printStackTrace();
-			}
-		});
+		messageService.getMethod(update).ifPresent(this::executeMethod);
+	}
+
+	private Message executeMethod(BotApiMethod<Message> method) {
+		Message execute = new Message();
+		try {
+			execute = execute(method);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+		return execute;
 	}
 
 	@Override
